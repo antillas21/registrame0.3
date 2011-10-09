8c19b6d1fe7527404520188b84c7380d80ce4e92
Antonio Antillon <antillas21@gmail.com>
2011-10-09 11:11:29 -0700

    Basic description of the Attendee model.

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -3,6 +3,20 @@ class Attendee
   include DataMapper::Resource
 
   property :id, Serial
-
-
+  property :first_name, String, :required => true, :index => [:full_name]
+  property :last_name, String, :required => true, :index => [:full_name]
+  property :email, String, :required => true, :index => true
+  property :phone, String, :required => false
+  
+  EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates_presence_of :first_name, :last_name, :email
+  validates_format_of :email, :with => EmailRegex
+  validates_uniqueness_of :email
+  validates_numericality_of :phone, :allow_nil => true
+  
+  def full_name
+    [first_name, last_name].join(' ')
+  end
+  
 end
--- spec/models/attendee_spec.rb
+++ spec/models/attendee_spec.rb
@@ -1,5 +1,75 @@
 require 'spec_helper'
 
 describe Attendee do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '#validations' do
+    it "should have a first name" do
+      @attendee = Attendee.new(:email => 'homer@springfield.com', :last_name => 'Simpson')
+      @attendee.valid?.should == false
+    end
+    
+    it "should have a last name" do
+      @attendee = Attendee.new(:email => 'homer@springfield.com', :first_name => 'Homer')
+      @attendee.valid?.should == false
+    end
+    
+    it "has a full name" do
+      @attendee = Attendee.create(
+        :first_name => 'Homer', :last_name => 'Simpson', :email => 'homer@springfield.com'
+      )
+      @attendee.full_name.should == 'Homer Simpson'
+    end
+    
+    it "should have an email address" do
+      @attendee = Attendee.new(:first_name => 'Homer', :last_name => 'Simpson')
+      @attendee.valid?.should == false
+    end
+    
+    it "should have a valid email address" do
+      @homer = Attendee.new(
+        :first_name => 'Homer', :last_name => 'Simpson', :email => 'homer@springfield.com'
+      )
+      @moe = Attendee.new(
+        :first_name => 'Moe', :last_name => 'Zyslak', :email => 'moe!springfield.com'
+      )
+      @homer.valid?.should == true
+      @moe.valid?.should == false
+    end
+    
+    it "should have a unique email address" do
+      @homer = Attendee.create(
+        :first_name => 'Homer', :last_name => 'Simpson', :email => 'homer@springfield.com'
+      )
+      @barney = Attendee.new(
+        :first_name => 'Barney', :last_name => 'Gumble', :email => 'homer@springfield.com'
+      )
+      @barney.valid?.should == false
+    end
+    
+    it "may have a phone number" do
+      @attendee = Attendee.create(
+        :first_name => 'Homer', :last_name => 'Simpson', :email => 'homer@springfield.com',
+        :phone => '9091112222'
+      )
+      @attendee.valid?.should == true
+    end
+    
+    it "accepts only numbers in phone number field" do
+      @homer = Attendee.create(
+        :first_name => 'Homer', :last_name => 'Simpson', :email => 'homer@springfield.com',
+        :phone => '9091112222'
+      )
+      @barney = Attendee.new(
+        :first_name => 'Barney', :last_name => 'Gumble', :email => 'barney@springfield.com',
+        :phone => '909-111-2222'
+      )
+      @moe = Attendee.new(
+        :first_name => 'Moe', :last_name => 'Zyslack', :email => 'moe@springfield.com',
+        :phone => '780-CALL-MOE'
+      )
+      
+      @homer.valid?.should == true
+      @barney.valid?.should == false
+      @moe.valid?.should == false
+    end    
+  end
 end
