44d4ab35aaeefbc35863bd49c851154a2e7e657f
Antonio Antillon <antillas21@gmail.com>
2011-10-09 12:47:32 -0700

    basic description of the Company model.

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,6 +3,18 @@ class Company
   include DataMapper::Resource
 
   property :id, Serial
-
+  property :name, String, :length => 200, :required => true, :index => true
+  property :address, String, :length => 200
+  property :city, String, :length => 200, :required => true, :index => true
+  
+  validates_presence_of :name, :city
+  
+  def to_param
+    "#{id}-#{url_name(name)}".downcase
+  end
+  
+  def url_name(field)
+    field.gsub(/[^a-z0-9]+/i, '-').gsub(/-+$/i, '')
+  end
 
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe Company do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "#validations" do
+    it "should have a name to be valid" do
+      @company = Company.new(:address => '742 Evergreen Terrace', :city => 'Springfield')
+      @company.valid?.should == false
+    end
+    
+    it "should have a city to be valid" do
+      @company = Company.new(:name => 'Homer Internet Co.')
+      @company.valid?.should == false
+    end
+  end
+  
+  context "#formatted output" do
+    it "has a nice url in the app" do
+      @company = Company.create(:name => 'Homer Internet Co.', :city => 'Springfield')
+      @other_company = Company.create(:name => 'Kwikie Mart', :city => 'Springfield')
+      @another_company = Company.create(:name => 'Lewis Jr. and Sons', :city => 'Springfield')
+      
+      @company.to_param.should == "#{@company.id}-homer-internet-co"
+      @other_company.to_param.should == "#{@other_company.id}-kwikie-mart"
+      @another_company.to_param.should == "#{@another_company.id}-lewis-jr-and-sons"
+    end
+    
+    it "displays a business card type record" do
+      pending
+    end
+  end
 end
