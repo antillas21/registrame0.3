649963d093ca9258933e760be95ecf3c071b1d5e
Antonio Antillon <antillas21@gmail.com>
2012-01-13 21:06:38 -0800

    Manually setting cookies on Preferences change.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,23 @@
 class PagesController < ApplicationController
   respond_to :json
+  before_filter :set_cookies
 
   def index
     @msg = 'Welcome to registrame'
+    if session[:current_user].present?
+      @this_user = session[:current_user].user
+    end
   end
 
   def companies_autocomplete
     @companies = Company.all(:name.like => "%#{params[:term]}%", order: [:name.asc])
     respond_with @companies.map(&:name)
+  end
+
+  def set_cookies
+    prefs = Preference.first
+    cookies[:qrcode] = prefs.create_qrcode
+    cookies[:label] = prefs.print_label
   end
 
 end
