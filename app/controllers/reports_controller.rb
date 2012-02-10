e54b5adbf0492e438c0e7223a9c61350552ab51c
Antonio Antillon <antillas21@gmail.com>
2012-02-10 02:21:34 -0800

    Created report export method and added records per printed status window on Statistics.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,6 +3,7 @@ require 'munger'
 class ReportsController < ApplicationController
   def index
     @total_records = Attendee.count
+    attendees_per_status
 
     if State.count > 0
       attendees_per_state
@@ -12,6 +13,35 @@ class ReportsController < ApplicationController
     end
     if AttendeeType.count > 0
       attendees_per_attendee_type
+    end
+  end
+
+  def show
+    export_csv
+  end
+
+  def export_csv
+    attendee_report
+    @csv = CSV.generate do |csv|
+      @att_collection.each do |row|
+        csv << row
+      end
+    end
+
+    created_at = Time.now.strftime("%Y%m%e-%H%M%S")
+    send_data @csv, type: 'text/plain', filename: "attendee-report-#{created_at}.csv", disposition: 'attachment'
+  end
+
+  def attendee_report
+    @attendees = Attendee.all
+    @att_collection = [
+      ['Registration Type', 'Full Name', 'Email', 'Phone', 'Company', 'Address', 'City', 'State', 'Country', 'Attended']
+    ]
+    @attendees.each do |attendee|
+      @att_collection << [
+          attendee.registration.to_s, attendee.full_name, attendee.email, attendee.phone, attendee.company_name.to_s,
+          attendee.address, attendee.city, attendee.state_name.to_s, attendee.country_name.to_s, attendee.printed.to_s
+        ]
     end
   end
 
@@ -72,5 +102,17 @@ class ReportsController < ApplicationController
 
     report = Munger::Report.from_data(q).sort('Country').process
     @country_report = Munger::Render.to_html(report)
+  end
+
+  def attendees_per_status
+    results = Attendee.aggregate(:printed, :all.count)
+    q = []
+
+    results.each do |row|
+      q << {'Printed' => row[0].to_s, 'Total' => row[1]}
+    end
+
+    report = Munger::Report.from_data(q).sort('Printed').process
+    @status_report = Munger::Render.to_html(report)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Registrame::Application.routes.draw do
   end
 
   match 'pages/companies_autocomplete' => 'pages#companies_autocomplete', :as => :companies_autocomplete
+  match 'reports/export' => 'reports#export_csv', :as => :report_export
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
